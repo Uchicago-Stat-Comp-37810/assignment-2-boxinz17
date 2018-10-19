af859cfdae7c2174e1cb7aded2b61e1880b9d79c
boxinz17 <boxinz@uchicago.edu>
2018-10-19 11:17:36 -0500

    Question5 supplement: Bug fix of infinite number
    modified run_metropolis_MCMC function to fix a bug. When the log posterior is too small (near negative infnite), we just set probab equal zero, rather than calculating it by exponential function, which may cause a bug sometimes.

--- run_metropolis_MCMC.R
+++ run_metropolis_MCMC.R
@@ -4,11 +4,15 @@ run_metropolis_MCMC <- function(startvalue, iterations){  # define the MH sampli
   for (i in 1:iterations){  # use the for loop to draw samples
     proposal = proposalfunction(chain[i,])  # use proposal function to draw (i+1)th sample based on the ith sample
     
-    probab = exp(posterior(proposal) - posterior(chain[i,]))  # calculate the acceptance probability. Because the symmetry property of the normal distribution, the proposal probability just cancelled. We only need to calculate the the ratio of target distribution.
+    if(is.infinite(posterior(proposal))){
+      probab <- 0
+    } else {
+      probab <- exp(posterior(proposal) - posterior(chain[i,]))  # calculate the acceptance probability. Because the symmetry property of the normal distribution, the proposal probability just cancelled. We only need to calculate the the ratio of target distribution.
+    }
     if (runif(1) < probab){  # use a uniform random variable between 0 and 1 to decide whether to accept the sample or not, so that the acceptance rate will be the desired probability
-      chain[i+1,] = proposal  # if the uniform random variable is smaller than the acceptance probability, then we accept this sample and put it into chain
+      chain[i+1,] <- proposal  # if the uniform random variable is smaller than the acceptance probability, then we accept this sample and put it into chain
     }else{
-      chain[i+1,] = chain[i,]  # otherwise, we drop this sample, set the (i+1)th sample the same as the ith sample
+      chain[i+1,] <- chain[i,]  # otherwise, we drop this sample, set the (i+1)th sample the same as the ith sample
     }
   }
   return(chain)  # after iterating for setted times, we return the whole chain
